76d68ee23fbe637267b5f3cdd44195fbfe696aa6
Andrii Snurnikov <78219181+y3Runda@users.noreply.github.com>
2021-12-30 01:26:42 +0200

    Initial commit

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -3,6 +3,7 @@
 @section('title', $post->title)
 
 @section('content')
+    @include('includes.categories')
     <div class="container">
         <h1 class="mt-5 mb-4 text-center">{{$post->title}}</h1>
         <a href="/" class="btn btn-outline-primary mb-4">Back</a>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,11 +3,7 @@
 @section('title', 'MyBlog')
 
 @section('content')
-    <div class="btn-group mb-4" role="group" aria-label="Basic outlined example">
-        @foreach($categories as $category)
-        <a href="{{route('getPostsByCategory', $category['slug'])}}" class="btn btn-outline-primary">{{$category['title']}}</a>
-        @endforeach
-    </div>
+    @include('includes.categories')
     @foreach($posts as $post)
     <div class="card mb-4">
         <div class="card-header">
@@ -22,13 +18,4 @@
     @endforeach
 
     {{$posts->links('vendor.pagination.bootstrap-4')}}
-    <!--<nav aria-label="Page navigation example">
-        <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-        </ul>
-    </nav>-->
 @endsection
