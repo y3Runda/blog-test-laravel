13f3a23023d305e6e252b11bfe1b4471f8354523
Andrii Snurnikov <78219181+y3Runda@users.noreply.github.com>
2021-12-30 01:30:34 +0200

    Initial commit

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -6,7 +6,7 @@
     @include('includes.categories')
     <div class="container">
         <h1 class="mt-5 mb-4 text-center">{{$post->title}}</h1>
-        <a href="/" class="btn btn-outline-primary mb-4">Back</a>
+        <a href="{{route('getPostsByCategory', $slug_category)}}" class="btn btn-outline-primary mb-4">Back</a>
         <article>
             {!! $post->text !!}
         </article>
